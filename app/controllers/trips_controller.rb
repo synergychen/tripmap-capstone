7f38297ae494ad248893465e74dab357adeed4de
Chen Huang <alan.tolearn@gmail.com>
2014-11-17 12:37:28 -0500

    User can create a new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,4 +2,23 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new
+    if @trip.update(trip_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:date, :city, :completed)
+  end
 end
