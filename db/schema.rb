2e55832885a01a393fab9e4bac04a00fb7957800
Chen Huang <alan.tolearn@gmail.com>
2014-11-17 11:11:30 -0500

    User can view all trips on index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117151319) do
+ActiveRecord::Schema.define(version: 20141117155904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: true do |t|
+    t.date     "date"
+    t.string   "city"
+    t.boolean  "completed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
