3ece57a5ddc3c7a7631b327a4c1e811b3ad3c113
Chen Huang <alan.tolearn@gmail.com>
2014-11-18 13:18:02 -0500

    User can view all locations of a trip

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,23 @@ class Location < ActiveRecord::Base
 
   has_many :trip_location_relationships
   has_many :trips, through: :trip_location_relationships
+
+  def order_of(trip)
+    trip_loc_relation = load_trip_location_relationship(trip)
+    trip_loc_relation.order
+  end
+
+  def stay_time_of(trip)
+    trip_loc_relation = load_trip_location_relationship(trip)
+    trip_loc_relation.stay_time
+  end
+
+  private
+
+  def load_trip_location_relationship(trip)
+    TripLocationRelationship.where(
+      trip_id: trip.id,
+      location_id: id
+    ).first
+  end
 end
