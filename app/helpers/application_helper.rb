4c3c92beb22c50a24a1d829041fa1e6026843045
Chen Huang <alan.tolearn@gmail.com>
2014-11-21 15:16:03 -0500

    Extract mode constant to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  TRANSPORTATION_MODE = ["WALKING", "TRANSIT", "DRIVING", "BICYCLING"]
+  WAYPOINTS_TRANS_MODE = [["Walking", "WALKING"], ["Driving", "DRIVING"],
+                          ["Bicycling", "BICYCLING"]]
 end
