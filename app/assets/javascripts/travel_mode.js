ae5f1b9c3d3759b51c4a6a9edd14da1859df18a3
Chen Huang <alan.tolearn@gmail.com>
2014-11-21 20:42:27 -0500

    Calculate and display total distance and time
    * Remove uniqueness of stop index [:trip_id, :location_id]
    * Calculate and display total distance
    * Calculate and display total time spent on road
    * Calculate and display total stay time of all stops

--- app/assets/javascripts/travel_mode.js
+++ app/assets/javascripts/travel_mode.js
@@ -1,17 +1,18 @@
 var startAndEndStop = [];
-var waypts = [];
+var wayPoints = [];
 var distances = [];
 var durations = [];
+var stayTimes = [];
 
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 
 $(function() {
   initialize();
-  calcAndDrawAllRoute(displayDistanceAndTime);
+  calcAndDrawAllRoute(displayDistanceAndDuration);
 
-  $("#mode-select-tag").change(function(){ 
-    calcAndDrawAllRoute(displayDistanceAndTime);
+  $("#mode-select-tag").change(function(){
+    calcAndDrawAllRoute(displayDistanceAndDuration);
   });
 })
 
@@ -29,11 +30,13 @@ function getStopsAndWaypoints() {
   $.each($(".trip-table-row"), function(i, trip) {
     var stopAddress = $(trip).find(".location-address").html();
     var address = stopAddress + ", " + tripCity
+    var stayTime = $(trip).find(".stop-stay_time").html();
 
     if ( i === 0 || i === stopNumber - 1 ) {
       startAndEndStop.push(address);
     } else {
-      waypts.push({location: address});
+      wayPoints.push({location: address})
+      stayTimes.push(parseInt(stayTime, 10));
     }
   });
 }
@@ -44,7 +47,7 @@ function calcAndDrawAllRoute(callback) {
   var request = {
       origin: startAndEndStop[0],
       destination: startAndEndStop[1],
-      waypoints: waypts,
+      waypoints: wayPoints,
       optimizeWaypoints: true,
       travelMode: google.maps.TravelMode[selectedMode]
   };
@@ -58,18 +61,52 @@ function calcAndDrawAllRoute(callback) {
       durations = [];
 
       for (var i = 0; i < route.legs.length; i++) {
-        distances.push(route.legs[i].distance.text);
-        durations.push(route.legs[i].duration.text);
+        distances.push(route.legs[i].distance.value);
+        durations.push(route.legs[i].duration.value);
       }
     }
     callback();
   });
 }
 
-function displayDistanceAndTime() {
-  var calcDistances = $(".calculated-distances");
-  var calcTimes = $(".calculated-times");
+function displayDistanceAndDuration() {
+  var distanceSummary = $(".distance-summary");
+  var durationSummary = $(".duration-summary");
+  var stayTimeSummary = $(".stay_time-summary");
 
-  calcDistances.html(distances);
-  calcTimes.html(durations);
+  distanceSummary.html(totalDistance());
+  durationSummary.html(totalTimeOnRoad());
+  stayTimeSummary.html(totalStayTime());
+}
+
+function totalDistance() {
+  var distanceSum = 0;
+
+  for ( var i = 0; i < distances.length; i++ ) {
+    distanceSum += distances[i];
+  }
+
+  return "Total Distance: " + distanceSum + " meters";
+}
+
+function totalTimeOnRoad() {
+  var durationSum = 0;
+
+  for ( var i = 0; i < distances.length; i++ ) {
+    durationSum += durations[i];
+  }
+
+  durationInMinutes = parseInt(durationSum/60, 10);
+
+  return "Total Time On Road: " + durationInMinutes + " minutes";
+}
+
+function totalStayTime() {
+  var stayTimeSum = 0;
+
+  for ( var i = 0; i < stayTimes.length; i++ ) {
+    stayTimeSum += stayTimes[i];
+  }
+
+  return "Total Stay Time: " + stayTimeSum + " minutes";
 }
