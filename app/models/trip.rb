b6d5ea03f7c892e93dddb94c9d678212f779350c
Chen Huang <alan.tolearn@gmail.com>
2014-11-24 15:39:58 -0500

    Generate TripGroup model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,8 @@ class Trip < ActiveRecord::Base
   validates :starts_on, presence: true
   validates :city, presence: true
 
-  belongs_to :user
+  has_many :trip_groups
+  has_many :users, through: :trip_groups
 
   has_many :stops
   has_many :locations, through: :stops
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
-  has_many :trips
+  has_many :trip_groups
+  has_many :trips, through: :trip_groups
 end
