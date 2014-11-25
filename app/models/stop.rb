83352f3cb6e43e672b1e0ddf9e79ac81a6dee43b
Chen Huang <alan.tolearn@gmail.com>
2014-11-25 15:24:44 -0500

    Add error messages; Add validates to stop

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,8 @@
 class Stop < ActiveRecord::Base
+  validates :order, presence: true
+  validates :stay_time, presence: true
+  validates :transportation_mode, presence: true
+
   belongs_to :trip
   belongs_to :location
 
