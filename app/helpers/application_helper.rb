f9ab5f5ec2b02e58bbb76530eb7cacc9fecce8e7
Chen Huang <alan.tolearn@gmail.com>
2014-12-04 16:43:03 -0500

    Extract mode select tag to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   TRANSPORTATION_MODE = ["WALKING", "TRANSIT", "DRIVING", "BICYCLING"]
   WAYPOINTS_TRANS_MODE = [["Walking", "WALKING"], ["Driving", "DRIVING"],
                           ["Bicycling", "BICYCLING"]]
+
+  def mode_select
+    select_tag "mode-select-tag", options_for_select(WAYPOINTS_TRANS_MODE)
+  end
 end
